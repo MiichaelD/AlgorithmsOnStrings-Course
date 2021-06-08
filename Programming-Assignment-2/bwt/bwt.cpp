a4515147592f785995f0827abdb5c1ddb6d9f86c
Michael Duarte <michael.duarte@hotmail.com>
2021-06-08 11:25:24 -0700

    Add Borrows-Wheeler Transform

--- Programming-Assignment-2/bwt/bwt.cpp
+++ Programming-Assignment-2/bwt/bwt.cpp
@@ -1,20 +1,51 @@
 #include <algorithm>
+#include <deque>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
+using namespace std;
+
+void printMatrix(const vector<string> &matrix) {
+  for (const string &row : matrix) {
+    cout << row << endl;
+  }
+}
+
+string getLastColumn(const vector<string> &matrix) {
+  std::stringstream result;
+  for (const string &row : matrix) {
+    result << row.back();
+  }
+  return result.str();
+}
+
+int safeIndex(const string &text, int index) {
+  int textLen = text.size();
+  if (index >= textLen) {
+    return (index - textLen);
+  }
+  if (index < 0) {
+    return (textLen + index);
+  }
+  return index;
+}
 
 string BWT(const string& text) {
-  string result = "";
-
-  // write your code here
-
-  return result;
+  vector<string> matrix;
+  int startIndex = text.find('$');
+  for (int i = 0; i < text.size(); ++i) {
+    std::stringstream ss;
+    for (int j = 0; j < text.size(); ++j) {
+      ss << text.at(safeIndex(text, j + startIndex));
+    }
+    matrix.emplace_back(ss.str());
+    ss.clear();
+    --startIndex;
+  }
+  printMatrix(matrix);
+  return getLastColumn(matrix);
 }
 
 int main() {
